8e377d6034710c53dd9423575cdec7addef8c0ab
Omer Bekin <omerbekinn@gmail.com>
2022-11-26 14:07:33 +0300

    Finish exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversedString = "";
+  for (let i = 0; i < string.length; i++) {
+    reversedString =  reversedString + string.charAt(string.length - (i + 1));
+  }  
+  return reversedString
 };
 
 // Do not edit below this line
