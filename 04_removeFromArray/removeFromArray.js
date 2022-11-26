b3763b1f18679cb46999064d5180f5b5b85f11bf
Omer Bekin <omerbekinn@gmail.com>
2022-11-26 15:23:40 +0300

    Finish exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+    //array.splice(index - 1, 1);
+    //return array
 
-};
+    args.forEach((arg) => {
+        const index = array.indexOf(arg);
+        if (index > -1) {
+            array.splice(index, 1);
+            
+        }
+    });
+    return array;
+}
 
-// Do not edit below this line
-module.exports = removeFromArray;
+    // Do not edit below this line
+    module.exports = removeFromArray;
