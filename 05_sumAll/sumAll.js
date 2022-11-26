9776bae4bbf56f99d4ad83e0831b57846c5f28cd
Omer Bekin <omerbekinn@gmail.com>
2022-11-26 15:38:47 +0300

    Finish exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(number0, number1) {
+    
+    if (!(typeof number0 === "number") || !(typeof number1 === "number")) {
+        return "ERROR"
+    }
 
+    if (number0 < 0 || number1 < 0) {
+        return "ERROR"
+    }
+
+    //if larger number first
+    if (number0 > number1) {
+        let number2 = number1;
+        number1 = number0;
+        number0 = number2;
+    }
+    
+    let sumAllNumbers = 0;
+    for (let i = number0; i <= number1; i++) {
+        sumAllNumbers = sumAllNumbers + i;
+    }
+    return sumAllNumbers;
 };
 
 // Do not edit below this line
