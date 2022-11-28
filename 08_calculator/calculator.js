809857d588bedc85b2b5d7a8332d66628bd84fe4
Omer Bekin <omerbekinn@gmail.com>
2022-11-28 17:25:40 +0300

    Finish exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {	
+
+  let total = 0;
+
+for (const num of array) {
+  total += num;
+}
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let total = 1;
 
+  for (const num of array) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
