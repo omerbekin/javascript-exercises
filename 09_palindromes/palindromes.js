c264cf6123ebfe566968fea589364f874d21b0c8
Omer Bekin <omerbekinn@gmail.com>
2022-11-28 17:55:03 +0300

    Finish exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleanString = (string.replace(/[!., ]/g, ``)).toLowerCase();
+    let reversedCleanString = ``;
 
+    for (let i = 0; i < cleanString.length; i++) {
+        reversedCleanString += cleanString.charAt(cleanString.length - (i + 1));
+    }
+
+    if(cleanString === reversedCleanString){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
