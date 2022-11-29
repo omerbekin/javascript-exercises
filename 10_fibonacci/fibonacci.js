476d57d312b4c13dbbd228eadf497486c0bff2c4
Omer Bekin <omerbekinn@gmail.com>
2022-11-29 14:15:37 +0300

    Finish exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function (fibonacciIndex) {
+    
+    if (fibonacciIndex < 0) {
+        return "OOPS"
+    }
+    
+    //let fibonacciArrayExample = [1, 1, 2, 3, 5, 8, 13];
+    let fibonacciArray = [];
+    let previousNumber1 = 0;
+    let previousNumber2 = 0;
+
+    for (let i = 0; i < fibonacciIndex; i++) {
+
+        
+        if (fibonacciArray.length === 0) {
+            fibonacciArray.push(1);
+        } else if (fibonacciArray.length === 1) {
+            fibonacciArray.push(1);
+        } else if (fibonacciArray.length >= 2) {
+            previousNumber1 = fibonacciArray[i - 2];
+            previousNumber2 = fibonacciArray[i - 1];
+            fibonacciArray.push(previousNumber1 + previousNumber2);
+        }
+    }
+
+    return fibonacciArray[fibonacciIndex - 1];
 
 };
 
