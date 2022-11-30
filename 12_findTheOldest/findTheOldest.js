6d037682f75f1a3575fcc6da65c19fa28b85c561
Omer Bekin <omerbekinn@gmail.com>
2022-11-30 15:14:57 +0300

    Finish exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let currentPersonAge;
 
+    let oldestPersonAge = 0;
+    let oldestPerson;
+
+    for (const person of people) {
+
+        let birthYear = person.yearOfBirth;
+        yearOfDeath = person.yearOfDeath;
+
+        if (typeof yearOfDeath !== 'undefined') {
+            let deathYear = person.yearOfDeath;
+            currentPersonAge = deathYear - birthYear;
+        } else {
+            let currentYear = new Date().getFullYear();
+            currentPersonAge = currentYear - birthYear;
+        }
+        if (currentPersonAge > oldestPersonAge) {
+            oldestPersonAge = currentPersonAge;
+            oldestPerson = person;
+        }
+    }
+    
+    return oldestPerson;
 };
 
 // Do not edit below this line
